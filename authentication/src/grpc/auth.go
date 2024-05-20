e70593211981951cb940eb9ced6e76910f242c40
Gabriel Trzimajewski <95551770+Sn0wye@users.noreply.github.com>
2024-05-20 20:59:24 -0300

    feat: parse token

--- authentication/src/grpc/auth.go
+++ authentication/src/grpc/auth.go
@@ -2,6 +2,7 @@ package grpc
 
 import (
 	"context"
+	"time"
 	"whatisthis/pb"
 	"whatisthis/pkg/config"
 	"whatisthis/pkg/jwt"
@@ -13,16 +14,35 @@ type authService struct {
 	pb.UnimplementedAuthServiceServer
 }
 
-func (s *authService) ValidateToken(ctx context.Context, req *pb.TokenRequest) (*pb.TokenResponse, error) {
+func (s *authService) ValidateToken(ctx context.Context, req *pb.ValidateTokenRequest) (*pb.ValidateTokenResponse, error) {
 	conf := config.GetConfig()
 	jwt := jwt.NewJwt(conf)
 
 	_, err := jwt.ParseToken(req.Token)
+
 	if err != nil {
-		return &pb.TokenResponse{Valid: false}, err
+		return &pb.ValidateTokenResponse{Valid: false}, err
 	}
 
-	return &pb.TokenResponse{Valid: true}, nil
+	return &pb.ValidateTokenResponse{Valid: true}, nil
+}
+
+func (s *authService) ParseToken(ctx context.Context, req *pb.ParseTokenRequest) (*pb.ParseTokenResponse, error) {
+	conf := config.GetConfig()
+	jwt := jwt.NewJwt(conf)
+
+	claims, err := jwt.ParseToken(req.Token)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.ParseTokenResponse{
+		Sub: claims.Subject,
+		Iss: claims.Issuer,
+		Iat: claims.IssuedAt.Format(time.RFC3339),
+		Exp: claims.ExpiresAt.Format(time.RFC3339),
+	}, nil
 }
 
 func RegisterAuthService(s *grpc.Server) {
