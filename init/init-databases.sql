3f5a8c9c238352f5ff7059fe4b104270eae184f3
Gabriel Trzimajewski <95551770+Sn0wye@users.noreply.github.com>
2024-12-22 15:28:22 +0200

    fix: init scripts

--- init/init-databases.sql
+++ init/init-databases.sql
@@ -9,3 +9,9 @@ SELECT 'CREATE DATABASE scorer'
 WHERE NOT EXISTS (
     SELECT FROM pg_database WHERE datname = 'scorer'
 )\gexec
+
+-- Create 'loan' database if it does not exist
+SELECT 'CREATE DATABASE loan'
+WHERE NOT EXISTS (
+    SELECT FROM pg_database WHERE datname = 'loan'
+)\gexec
